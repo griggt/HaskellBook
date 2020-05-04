a641d5622b58ec029d4037848f8d06ed121fabae
Tom Grigg <tomegrigg@gmail.com>
2020-05-04 11:44:51 -0700

    ch03: print3Broken.hs

---
+++ ch03/print3Broken.hs
@@ -0,0 +1,13 @@
+module Print3Broken where
+
+--greeting = "Yarrrr"
+
+printSecond :: IO ()
+printSecond = do
+    putStrLn greeting
+
+main :: IO ()
+main = do
+    putStrLn greeting
+    printSecond
+    where greeting = "Yarrrr"
